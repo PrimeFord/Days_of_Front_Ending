7688232f896f73e6aaabce7fab5f60cb6e1e004c
Prime Senpai <Bashfordbash@gmail.com>
2024-02-05 22:03:00 +0100

    feat: Clock

--- 25 + 5 Clock/main.js
+++ 25 + 5 Clock/main.js
@@ -1,5 +1,21 @@
 function App() {
-  return <div>Clock</div>;
+  return (
+    <div>
+      <div id="title"></div>
+      <div id="label">
+        <div id="break-label">
+          <button id="break-decrement"></button>
+          <div id="break-length">5</div>
+          <button id="break-increment"></button>
+        </div>
+        <div id="session-label">
+          <button id="session-decrement"></button>
+          <div id="session-length">25</div>
+          <button id="session-increment"></button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
