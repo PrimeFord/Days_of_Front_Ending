f0ebbc3492c39d8b8749db52d5f958e9f3bad46b
Prime Senpai <Bashfordbash@gmail.com>
2024-02-10 22:50:44 +0100

    feat: 25 + 5 Clock

--- 25 + 5 Clock/main.js
+++ 25 + 5 Clock/main.js
@@ -1,18 +1,45 @@
 function App() {
   return (
-    <div>
-      <div id="title"></div>
+    <div id="board">
+      <div id="title">25 + 5 Clock</div>
       <div id="label">
-        <div id="break-label">
-          <button id="break-decrement"></button>
-          <div id="break-length">5</div>
-          <button id="break-increment"></button>
+        <div>
+          Break Length
+          <div id="break-label">
+            <button id="break-decrement">
+              <i class="fa fa-arrow-down"></i>
+            </button>
+            <div id="break-length">5</div>
+            <button id="break-increment">
+              <i class="fa fa-arrow-up"></i>
+            </button>
+          </div>
         </div>
-        <div id="session-label">
-          <button id="session-decrement"></button>
-          <div id="session-length">25</div>
-          <button id="session-increment"></button>
+        <div>
+          Session Length
+          <div id="session-label">
+            <button id="session-decrement">
+              <i class="fa fa-arrow-down"></i>
+            </button>
+            <div id="session-length">25</div>
+            <button id="session-increment">
+              <i class="fa fa-arrow-up"></i>
+            </button>
+          </div>
         </div>
+      </div>
+      <div id="display">
+        <div id="timer-label">Session</div>
+        <div id="time-left">25:00</div>
+      </div>
+      <div id="control">
+        <button id="start_stop">
+          <i class="fa fa-play"></i>
+          <i class="fa fa-pause"></i>
+        </button>
+        <button id="reset">
+          <i class="fa fa-arrows-rotate"></i>
+        </button>
       </div>
     </div>
   );
